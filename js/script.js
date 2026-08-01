cbf3aa0e9562238519d253c099080e9677993583
Yuke <giovanamarinhocanalli@gmail.com>
2026-08-01 13:42:35 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,366 @@
+
+const livros = {
+
+    crime: {
+        titulo: "Crime e Castigo",
+
+        capa: "imgs/capacrimeecastigo.jpg",
+
+        sinopse: `Um estudante apresentado como Rodion Romanovitch
+                 Raskolnikov possui um ideal que ele acredita fortemente portanto, ao realizar um crime
+                que ele afirma ter sido um teste para testar sua teoria, ele começa a sofrer as consequências da mente humana
+                onde ele fica dividido entre culpa e a razão.`,
+
+        opiniao: `É um ótimo livro para quem busca algo mais psicologico, Fiodor faz questão 
+                de fazer o leitor acompanhar cada pensamento e ato do 
+                protagonista, desde o começo e desde a sua redenção (mentalmente dizendo).`,
+
+        nota: "★★★★★",
+
+        obs: `Um livro muito legal de se acompanhar, e para quem gosta de acompanhar e
+                 analisar os personagens tambem vai achar legal`,
+
+        sticker: "imgs/sticker.png"
+
+        
+    },
+
+    idiota: {
+        titulo: "Idiota",
+
+        capa: "imgs/capaidiota.jpg",
+
+        sinopse: `narra a história do jovem Príncipe Míchkin, que, após vários anos
+         em um sanatório na Suíça, regressa à Rússia e se depara com um
+          mundo atolado na indolência material cotidiana`,
+
+        opiniao: `por ser novamente do mesmo autor que escreveu Crime e Castigo, o livro é muito bom, mas não
+         chega a ser tão bom quanto o outro, mas ainda assim é um ótimo livro para quem gosta de acompanhar a mente humana`,
+
+        nota: "★★★★",
+
+        obs: `Um livro muito legal de se acompanhar, e carrega uma baita critica social tambem, mas não é tão bom quanto Crime e Castigo (meu querido)`,
+
+        sticker: "imgs/sticker.png"
+
+        
+    },
+
+    irmaos: {
+        titulo: "Irmãos Karamazov",
+
+        capa: "imgs/capairmaos.jpg",
+
+        sinopse: `O livro infame do Dostoievski, que afirmam ser o melhor romance do autor,
+        o livro narra a história de três irmãos que são muito diferentes entre si, e que acabam se envolvendo em 
+        um crime que abala a cidade inteira (todos os livros do dostoievski sempre envolve crimeKKKK)`,
+
+        opiniao: `O Fiodor acertou em cheio nesse livro, apesar de eu ainda não cheguei a terminar (livro longo)
+        o livro apresenta ideias muito interessantes para se refletir (como eu adoro refletir eu adorei kkkk)`,
+
+        nota: "★★★★★",
+
+        obs: `mas continuo adorando mais crime e castigoKKKKK`,
+
+        sticker: "imgs/sticker.png"
+
+        
+    },
+
+    reideamarelo: {
+        titulo: "O Rei de Amarelo",
+
+        capa: "imgs/reideamarelocapa.webp",
+
+        sinopse: `Acredito que quem ja tenha visto args ou jogos mais de terror psicologico claramente conhece esse livro,
+        é um livro que apresenta a trama do Rei apartir de contos, mas claro, o autor não deixa muito claro o que é e etc,
+        deixando a conclusão especialmente para o leitor, onde surge muitas teorias da aparencia, dos atos e de Carcosa.`,
+
+        opiniao: `É confuso, mas para quem gosta bastante de teorizar as coisas, esse livro é para você.
+        o unico problema é que é um pouco curto, mas nada que impeça de ser um bom livro.`,
+
+        nota: "★★★★★",
+
+        obs: `eu adoro esse livro`,
+
+        sticker: "imgs/sticker.png"
+
+        
+    },
+
+    odisseia: {
+        titulo: "Odisseia",
+
+        capa: "imgs/odisseiacapa.webp",
+
+        sinopse: `Agora que vai ter o filme, acredito que esse livro ganhe ate mais popularidade, o poema
+        conta sobre Odisseu/Ulisses (varia bastante a tradução) e mostra a jornada da guerra ate a volta
+        para casa.`,
+
+        opiniao: `Para quem gosta de poemas, eu achei ele bem interessante, mas bati cabeça para ler por não ser muito direto e ficar
+        10 linhas descrevendo uma pessoa com palavras chiques...`,
+
+        nota: "★★★★",
+
+        obs: `mas eu adorei o conceito dele, o Homero pensou a frente do tempo deleKKKK`,
+
+        sticker: "imgs/sticker.png"
+
+        
+    },
+
+    fausto: {
+        titulo: "Fausto",
+
+        capa: "imgs/faustopt1.jpg",
+
+        sinopse: `Fausto, é um homem solitário e, apesar
+         de seus grandes conhecimentos sobre o mundo, acredita que a magia é a
+          resposta para os problemas espirituais. Com o diabo Mefisto, Fausto busca resolver
+           suas angústias, no entanto, acaba apenas criando novos problemas.`,
+
+        opiniao: `o conceito dele é extremamente interessante, so odiei o fato de ser poema, então cansa bem rapido, mas nao deixa de ter uma
+        boa historia.`,
+
+        nota: "★★★★",
+
+        obs: `so perdeu 1 estrela porque é poema e eu sou hater de poemas`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    fausto2: {
+        titulo: "Fausto - Parte 2",
+
+        capa: "imgs/faustopt2.jpg",
+
+        sinopse: `É basicamente a continuação do primeiro, bem mais longo e com vários acontecimentos que variam ao longo da historia.`,
+
+        opiniao: `por ser poema e longo, é uma tortura ler, mas eu adorei o conceito.`,
+
+        nota: "★★★",
+
+        obs: `1000 paginas = 1000 anos de sofrimento`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    ocapote: {
+        titulo: "O Capote",
+
+        capa: "imgs/ocapote.jpg",
+
+        sinopse: `O Capote é um romance que retrata a vida de um homem que se sente isolado e descontente com sua existência.
+        mas alem de tudo... ele precisa de um capote novo.`,
+
+        opiniao: `O livro é legal, mas meio que ao mesmo tempo engraçado pelo fato do final ser inesperado para um carambaKKKKK.`,
+
+        nota: "★★★★★",
+
+        obs: `ainda sim aborda temas de filosofia como solidão, identidade e claramente a busca por um capote.`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    memorias: {
+        titulo: "memorias do subsolo",
+
+        capa: "imgs/memorias.jpg",
+
+        sinopse: `Outro livro do nosso amigo Dostoievski, onde o livro narra a vida de um homem que interage com o leitor as vezes e que se sente
+        profundamente descontente em viver, nao vendo nenhum sentido em sua existencia`,
+
+        opiniao: `Protagonista é um cara chato pra caramba`,
+
+        nota: "★★★★",
+
+        obs: `Mas é legalKKKK`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    metamorfose: {
+        titulo: "A Metamorfose",
+
+        capa: "imgs/metamorfose.webp",
+
+        sinopse: `Um classico, onde relata o conto onde Gregor Samsa acorda como uma fucking barata,
+        mas o principal ponto do livro é criticar a sociedade e como ela trata as pessoas que não se encaixam no padrão.`,
+
+        opiniao: `Eu achei esse livro incrivel pois apesar de ser meio antigo e curto, a critica ainda permanece,
+        o que pra mim é incrivel`,
+
+        nota: "★★★★",
+
+        obs: `eu tenho medo de baratas uiuiui`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    inferno: {
+        titulo: "A Divina Comédia - Inferno",
+
+        capa: "imgs/comedia.jpg",
+
+        sinopse: `Uns dos livros que mais influenciou a literatura, o livro narra a historia do proprio autor
+        vagando pelo inferno, purgatorio e paraiso acompanhado de Vergillius e Beatriz.`,
+
+        opiniao: `EU AMO ESSE LIVRO AIAIAIAIAIAIQAIFAIASJIAISIDQIUJWHNDJ
+        ele é incrivel, o conceito é incrivel, a gente so ignora que é poema com um milhão de referencias politicas`,
+
+        nota: "★★★★★",
+
+        obs: `o melhor e o mais famoso dos três.`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    purgatorio: {
+        titulo: "A Divina Comédia - Purgatório",
+
+        capa: "imgs/comedia.jpg",
+
+        sinopse: `Aqui o Dante sai do inferno e vai para o purgatorio, onde encontra pecadores se purificando e mostrando
+        arrependimento genuino pelo seus atos, e que o purgatório é parte do caminho para o paraíso.`,
+
+        opiniao: `ELE É INCRIVEL TAMBEM, eu acho muito legal que voce pode ver isso nao de uma maneira religiosa,
+        mas tambem de maneira filosofica, envolvendo como a mente humana é e etc etc.`,
+
+        nota: "★★★★★",
+
+        obs: `o meu favorito justamente por poder reflitir sobre de maneira filosofica.`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    paraiso: {
+        titulo: "A Divina Comédia - Paraíso",
+
+        capa: "imgs/comedia.jpg",
+
+        sinopse: `aqui é onde Vergillius se despede como guia, e Beatriz guia Dante ao paraíso.`,
+
+        opiniao: `Ele é o mais filosofico dos três, e o mais dificil de ler, mas ainda assim é um livro muito legal,
+        e mostra uma visão muito famosa hoje em dia de Deus e os seus anjos.`,
+
+        nota: "★★★★★",
+
+        obs: `Conceito interessante, e eu amo historias que envolvem algum aspecto religioso.`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    mobydick: {
+        titulo: "Moby Dick",
+
+        capa: "imgs/mobydick.jpg",
+
+        sinopse: `É um clássico, onde Herman conta a história de Ishmael, que possui uma grande admiração por baleias, diferente
+        do Ahab que demonstra uma grande obsessão com a Moby Dick.`,
+
+        opiniao: `EU AMO ESSE LIVRO. ele pode ser engraçado tambem, e muita gente suspeita
+        do Ishmael e Queequeg serem um casal gay, mas pra mim tanto faz, o livro conta a obsessão de uma
+        maneira que faz a leitura ser cansativa`,
+
+        nota: "★★★★",
+
+        obs: `Denso demais, e tambem o Ishmael decide virar uma enciclopédia a cada 10 paginas.`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+    doriangray: {
+        titulo: "O Retrato de Dorian Gray",
+
+        capa: "imgs/dorian.jpg",
+
+        sinopse: `O livro conta sobre a história de Dorian gray, e o proprio livro te faz refletir sobre
+        moral, cultura e a sociedade.`,
+
+        opiniao: `De todos, esse foi o que mais me prendeu, eu adorei esse conceito do quadro,
+        e o autor nunca confirma o que realmente foi o quadro, o que deixa o conceito pro leitor.`,
+
+        nota: "★★★★★",
+
+        obs: `Uma pena que é curto.`,
+
+        sticker: "imgs/sticker.png"
+    
+    },
+
+};
+
+const Elementolivros = document.querySelectorAll(".livro");
+
+Elementolivros.forEach(livro => {
+
+    livro.addEventListener("click", () => {
+
+        window.location.href = "analise.html?livro=" + livro.id;
+
+        livro.classList.add("abrindo");
+
+        document.body.classList.add("saindo");
+
+        setTimeout(() => {
+
+            window.location.href = "analise.html?livro=" + livro.id;
+
+        }, 600);
+
+    });
+
+});
+
+const voltar = document.querySelector(".voltar");
+
+    voltar.addEventListener("click", (evento) => {
+
+        evento.preventDefault();
+
+        document.body.classList.add("saindo");
+
+        setTimeout(() => {
+
+            window.location.href = "index.html";
+
+    }, 1000);
+
+});
+
+
+const parametros = new URLSearchParams(window.location.search);
+
+const livroEscolhido = parametros.get("livro");
+
+const livro = livros[livroEscolhido];
+
+document.getElementById("titulo").innerHTML = livro.titulo;
+
+document.getElementById("capa").src = livro.capa;
+
+document.getElementById("sinopse").innerHTML = livro.sinopse;
+
+document.getElementById("opiniao").innerHTML = livro.opiniao;
+
+document.getElementById("obs").innerHTML = livro.obs;
+
+document.getElementById("nota").innerHTML = livro.nota;
+
+document.getElementById("sticker").src = livro.sticker;
+
+
+function abrirLivro(livro){
+
+    window.location.href = "analise.html?livro=" + livro;
+
+}
